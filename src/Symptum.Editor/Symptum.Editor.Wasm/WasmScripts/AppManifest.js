fd99545da0d86b309e15b8b637c0a088984326a4
Shankar <shankarbus04082001@hotmail.com>
2024-05-06 13:48:11 +0530

    Migration to Uno Platform Single Project

--- src/Symptum.Editor/Symptum.Editor.Wasm/WasmScripts/AppManifest.js
+++
@@ -1,3 +0,0 @@
-var UnoAppManifest = {
-    displayName: "Symptum Editor"
-}
--- src/Symptum/Symptum.Server/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-global using Microsoft.AspNetCore.Mvc;
-global using Symptum.DataContracts;
-global using Symptum.Server.Apis;
